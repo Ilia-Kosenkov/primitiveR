ef8d3b15f2e9ab105d58a9ed320ac1f97c4d281b
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-11-24 00:56:45 +0200

    Rewriting C code

--- src/code.cpp
+++ src/code.cpp
@@ -1,29 +1,29 @@
-#include "imports.h"
+#include <Rcpp.h>
+#include <cmath>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-SEXP primR_are_equal_f(SEXP x, SEXP y, SEXP eps) {
-	const auto x_vals = as<std::vector<double>>(x);
-	const auto y_vals = as<std::vector<double>>(y);
-	const auto eps_val = as<double>(eps);
+LogicalVector primR_are_equal_f(NumericVector x, NumericVector y, double eps) {
 
-	std::vector<bool> result(x_vals.size());
-	for (size_t i = 0; i < x_vals.size(); i++) {
-		if (std::isnan(x_vals[i]) || std::isnan(y_vals[i]))
+	const size_t len = x.length();
+	LogicalVector result(len);
+	
+	for (size_t i = 0; i < len; i++) {
+		if (std::isnan(x[i]) || std::isnan(y[i]))
 			result[i] = false;
-		else if (std::isinf(x_vals[i]) || std::isinf(y_vals[i]))
-			result[i] = x_vals[i] == y_vals[i];
+		else if (std::isinf(x[i]) || std::isinf(y[i]))
+			result[i] = x[i] == y[i];
 		else 
 		{
-			const auto x_abs = fabs(x_vals[i]);
-			const auto y_abs = fabs(y_vals[i]);
+			const auto x_abs = fabs(x[i]);
+			const auto y_abs = fabs(y[i]);
 
 			if (x_abs == 0.0)
-				result[i] = y_abs < eps_val;
+				result[i] = y_abs < eps;
 			else if (y_abs == 0.0)
-				result[i] = x_abs < eps_val;
+				result[i] = x_abs < eps;
 			else
-				result[i] = fabs(x_vals[i] - y_vals[i]) < eps_val * std::max(x_abs, y_abs);
+				result[i] = fabs(x[i] - y[i]) < eps * std::max(x_abs, y_abs);
 		}
 	}
 
